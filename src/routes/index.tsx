bda9650d27b853fa51a0a9adc9b4680376123881
ArafatSarker <mmarafatu@gmail.com>
2025-09-18 04:10:36 +0600

    i am connect user and task router its now workin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
 import App from "@/App";
+import Taska from "@/pages/Taska";
+import User from "@/pages/User";
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
     {
         path: "/",
         // element: <App></App>
-        Component: App
+        Component: App,
+        children:[
+            {
+                path:"user",
+                Component: User,
+            },
+            {
+                path:"task",
+                Component: Taska,
+            },
+        ]
     }
 ])
 
