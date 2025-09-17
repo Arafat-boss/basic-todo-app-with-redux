cb2d2ad3e1c9d3065e7196cac3a33039b7f9db8c
ArafatSarker <mmarafatu@gmail.com>
2025-09-18 03:48:29 +0600

    Now i am successfully create a router and connect react router dom with created router

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>I Love BAngladesh</div>
+    }
+])
+
+export default router;
