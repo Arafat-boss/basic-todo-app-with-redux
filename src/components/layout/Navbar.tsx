859f6fc82501646acf8ae0f51a13e0a01f250493
ArafatSarker <mmarafatu@gmail.com>
2025-09-20 04:43:13 +0600

    i am successfully added Handel dark mode using shadcn and it's successfully work

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,18 +1,23 @@
 import Logo from "@/assets/bag.png";
 import { Link } from "react-router";
-
+import { ModeToggle } from "../mode-toggle";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5 justify-between">
       <div className="flex items-center">
         <img className="w-[50px]" src={Logo} alt="" />
         <span className="font-bold ml-2">TaskMaster</span>
       </div>
+      <div className="px-2">
+        <Link className="px-2" to="/tasks">Tasks</Link>
+        <Link className="px-2" to="/users">Users</Link>
+        <Link className="px-2" to="/users">Create Users</Link>
+      </div>
 
-      <Link to="/tasks">Tasks</Link>
-      <Link to="/users">Users</Link>
-      <Link to="/users">Create Users</Link>
+      <div>
+        <ModeToggle></ModeToggle>
+      </div>
     </nav>
   );
 }
