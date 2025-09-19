04d9dced4a6b0020f390f40279f0ea6717bc69b7
ArafatSarker <mmarafatu@gmail.com>
2025-09-20 05:30:09 +0600

    now i am create a task Slice in redux

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5 justify-between">
       <div className="flex items-center">
         <img className="w-[50px]" src={Logo} alt="" />
-        <span className="font-bold ml-2">TaskMaster</span>
+        <span className="font-bold ml-2">Task Master</span>
       </div>
       <div className="px-2">
         <Link className="px-2" to="/tasks">Tasks</Link>
