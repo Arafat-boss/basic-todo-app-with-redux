732c8d93df76700a046463a83962ee26655148bc
ArafatSarker <mmarafatu@gmail.com>
2025-09-21 01:56:56 +0600

    i am forget a cretae a hook that's why i am not connect a tasks to todo but i am understend properly

--- src/pages/Taska.tsx
+++ src/pages/Taska.tsx
@@ -1,7 +1,10 @@
+import { useAppSelector } from '@/redux/'
 
 export default function Taska() {
 
   // const {} = useAppSelector((state) => state.todo.tasks)
+  const todo = useAppSelector((state) => state.todo.tasks)
+  console.log(todo);
 
   return (
     <div>Taska</div>
