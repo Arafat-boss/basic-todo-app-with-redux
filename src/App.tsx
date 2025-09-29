33be348e309151058bc3e04ef248aecdb6b5eb8a
Arafat Sarker <mmarafatu@gmail.com>
2025-09-30 03:40:52 +0600

    i am successfully done all funtionalyti

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Navbar from "./components/layout/Navbar";
 function App() {
   return (
     <>    
+    {/* // Navbar + Outlet for nested routes */}
       <Navbar />
-
       <div className="container mx-auto mt-10">
         <Outlet />
       </div>
